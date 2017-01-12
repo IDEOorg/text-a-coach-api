a56514c6ff8abbd3ed5859cb2f538606e9a51e33
Matt Winkler <matt@mattwinkler.com>
2017-01-12 14:15:49 -0800

    Bugfix: editable flavors

--- app/admin/flavor.rb
+++ app/admin/flavor.rb
@@ -1,6 +1,8 @@
 ActiveAdmin.register Flavor do
   menu false
 
+  permit_params :name, :handle
+
   sidebar "Flavor Contents", only: [:show, :edit] do
     ul do
       li link_to "Conversations",   admin_flavor_conversations_path(resource)
